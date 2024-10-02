19a56e5004566a2153ad8ca11626d8fbd88af76c
PaxCodez <160773169+PaxCodez@users.noreply.github.com>
2024-10-02 08:40:35 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+let Value = 0
+basic.forever(function () {
+    Value = pins.analogReadPin(AnalogReadWritePin.P1)
+    if (Value > 500) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    } else if (Value < 500) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+    }
+})
